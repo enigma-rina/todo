e5dad439275a3afc0f3867507366a540292e74d6
enigma_rina <mushroommar@gmail.com>
2023-03-28 23:21:16 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,77 @@
+(() => {
+  let toDoListArray = [];
+
+  const form = document.querySelector(".form");
+  const input = form.querySelector(".form__input");
+  const ul = document.querySelector(".toDoList");
+  const dateElement = document.querySelector(".date");
+
+  function displayDate() {
+    const today = new Date();
+    const dateString = today.toLocaleDateString();
+    dateElement.textContent = dateString;
+  }
+
+  function saveData() {
+    localStorage.setItem("toDoListArray", JSON.stringify(toDoListArray));
+  }
+
+  function loadData() {
+    const savedData = localStorage.getItem("toDoListArray");
+    if (savedData) {
+      toDoListArray = JSON.parse(savedData);
+      toDoListArray.forEach((item) => addItemToDOM(item.itemId, item.toDoItem, item.completed));
+    }
+  }
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let itemId = String(Date.now());
+    let toDoItem = input.value;
+    addItemToDOM(itemId, toDoItem);
+    addItemToArray(itemId, toDoItem);
+    input.value = "";
+    saveData();
+  });
+
+  ul.addEventListener("click", (e) => {
+    let id = e.target.getAttribute("data-id");
+    if (!id) return;
+    let item = toDoListArray.find((item) => item.itemId === id);
+    if (item.completed) {
+      removeItemFromDOM(id);
+      removeItemFromArray(id);
+    } else {
+      item.completed = true;
+      const li = document.querySelector('[data-id="' + id + '"]');
+      li.style.textDecoration = "line-through";
+    }
+    saveData();
+  });
+
+  function addItemToDOM(itemId, toDoItem, completed = false) {
+    const li = document.createElement("li");
+    li.setAttribute("data-id", itemId);
+    li.innerText = toDoItem;
+    if (completed) {
+      li.style.textDecoration = "line-through";
+    }
+    ul.appendChild(li);
+  }
+
+  function addItemToArray(itemId, toDoItem) {
+    toDoListArray.push({ itemId, toDoItem, completed: false });
+  }
+
+  function removeItemFromDOM(id) {
+    var li = document.querySelector('[data-id="' + id + '"]');
+    ul.removeChild(li);
+  }
+
+  function removeItemFromArray(id) {
+    toDoListArray = toDoListArray.filter((item) => item.itemId !== id);
+  }
+
+  displayDate();
+  loadData();
+})();
